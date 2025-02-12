78df7ce7905297947578f801f29a1437f152fed4
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2025-02-12 13:40:30 +1100

    Tweak Menuwhere config

--- setup.sh
+++ setup.sh
@@ -385,7 +385,7 @@ defaults_write com.google.Chrome NSUserKeyEquivalents -dict "New Tab" "@~t" "New
 defaults_write com.manytricks.Menuwhere "Application Mode" -int 2 # Run in faceless mode
 defaults_write com.manytricks.Menuwhere "Skip Disabled Menu Items" -bool true # Hide disabled menu items
 defaults_write com.manytricks.Menuwhere "Stealth Mode" -bool true # Don't show settings on launch
-defaults_write com.manytricks.Menuwhere Blacklist -string "Menuwhere" # Disable Menuwhere menu item
+defaults_write com.manytricks.Menuwhere Blacklist -string "Apple,Menuwhere" # Disable menu items for Apple and Menuwhere
 defaults_write com.manytricks.Menuwhere SUEnableAutomaticChecks -bool true # Enable automatic updates
 
 log "Configuring Menuwhere login item." # Run Menuwhere on login
