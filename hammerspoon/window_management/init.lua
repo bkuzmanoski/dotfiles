98b6414e11a1901e95f8eb0c4f9a884c08a35175
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2025-02-21 10:34:21 +1100

    Add focus_after_close to window_management

--- hammerspoon/window_management/init.lua
+++ hammerspoon/window_management/init.lua
@@ -3,6 +3,10 @@ local module = {}
 -- Focus the topmost window on the screen when the mouse enters it
 local screenFocus = require("window_management.screen_focus")
 
+-- Focus the frontmost remaining window on the screen when the focused window is closed
+-- (e.g. prevent the focus from moving to another space or the Desktop)
+local focusAfterClose = require("window_management.focus_after_close")
+
 -- Move focus to the window to the left or right of the focused window
 local focusLeftRight = require("window_management.focus_left_right")
 module.focusLeftRightHotkeys = focusLeftRight.hotkeys
@@ -20,6 +24,7 @@ module.tileHotkeys = tile.hotkeys
 
 function module.init()
   screenFocus.init()
+  focusAfterClose.init()
   focusLeftRight.init()
   moveAndResize.init()
   tile.init()
@@ -27,6 +32,7 @@ end
 
 function module.cleanup()
   screenFocus.cleanup()
+  focusAfterClose.cleanup()
   focusLeftRight.cleanup()
   moveAndResize.cleanup()
   tile.cleanup()
