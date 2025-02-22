9987cb1dc42257ae501cce0f85b43ff6b32081c2
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2025-02-22 21:00:23 +1100

    Update themes

--- sketchybar/plugins/datetime.sh
+++ sketchybar/plugins/datetime.sh
@@ -2,11 +2,5 @@
 
 source "${CONFIG_DIR}/colors.sh"
 
-if [[ $(defaults read NSGlobalDomain AppleInterfaceStyle 2> /dev/null) == "Dark" ]]; then
-  label_color="${LABEL_DARK}"
-else
-  label_color="${LABEL_LIGHT}"
-fi
-
-sketchybar --set date label="$(date '+%a %-d %b')" label.color="${label_color}"
-sketchybar --set time label="$(date '+%-I:%M %p' | tr '[:upper:]' '[:lower:]')" label.color="${label_color}"
+sketchybar --set date label="$(date '+%a %-d %b')" label.color="${LABEL_COLOR}"
+sketchybar --set time label="$(date '+%-I:%M %p' | tr '[:upper:]' '[:lower:]')" label.color="${LABEL_COLOR}"
--- sketchybar/plugins/next_event.sh
+++ sketchybar/plugins/next_event.sh
@@ -2,44 +2,28 @@
 
 source "${CONFIG_DIR}/colors.sh"
 
-if [[ $(defaults read NSGlobalDomain AppleInterfaceStyle 2> /dev/null) == "Dark" ]]; then
-  icon_color="${ICON_DARK}"
-  label_color="${LABEL_DARK}"
-  background_color="${BACKGROUND_DARK}"
-  background_hover_color="${BACKGROUND_HOVER_DARK}"
-  background_active_color="${BACKGROUND_ACTIVE_DARK}"
-  border_color="${BORDER_DARK}"
-else
-  icon_color="${ICON_LIGHT}"
-  label_color="${LABEL_LIGHT}"
-  background_color="${BACKGROUND_LIGHT}"
-  background_hover_color="${BACKGROUND_HOVER_LIGHT}"
-  background_active_color="${BACKGROUND_ACTIVE_LIGHT}"
-  border_color="${BORDER_LIGHT}"
-fi
-
 case "${SENDER}" in
   "appearance_change")
-    sketchybar --set "${NAME}" icon.color="${icon_color}" label.color="${label_color}" background.color="${background_color}" background.border_color="${border_color}"
+    sketchybar --set "${NAME}" icon.color="${ICON_COLOR}" label.color="${LABEL_COLOR}" background.color="${BACKGROUND_COLOR}" background.border_color="${BORDER_COLOR}"
     ;;
   "mouse.entered")
-    sketchybar --set "${NAME}" background.color="${background_hover_color}"
+    sketchybar --set "${NAME}" background.color="${BACKGROUND_HOVER_COLOR}"
     ;;
   "mouse.exited")
-    sketchybar --set "${NAME}" background.color="${background_color}"
+    sketchybar --set "${NAME}" background.color="${BACKGROUND_COLOR}"
     ;;
   "mouse.clicked")
-    sketchybar --set "${NAME}" background.color="${background_active_color}"
+    sketchybar --set "${NAME}" background.color="${BACKGROUND_ACTIVE_COLOR}"
     sleep 0.1
-    sketchybar --set "${NAME}" background.color="${background_hover_color}"
+    sketchybar --set "${NAME}" background.color="${BACKGROUND_HOVER_COLOR}"
     ;;
   *)
     next_event="$("${CONFIG_DIR}/helpers/GetNextEvent.swift")"
 
-    if [[ -z "${next_event}" ]]; then
+    if [[ $? -ne 0 ]] || [[ -z "${next_event}" ]]; then
       sketchybar --set "${NAME}" drawing=off
     else
-      sketchybar --set "${NAME}" drawing=on label="${next_event}" icon.color="${icon_color}" label.color="${label_color}" background.color="${background_color}" background.border_color="${border_color}"
+      sketchybar --set "${NAME}" drawing=on label="${next_event}" icon.color="${ICON_COLOR}" label.color="${LABEL_COLOR}" background.color="${BACKGROUND_COLOR}" background.border_color="${BORDER_COLOR}"
     fi
     ;;
 esac
