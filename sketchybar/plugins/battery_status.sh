098807239a106c0b3c2151e609643cb59e10a914
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2025-02-22 21:00:23 +1100

    Update themes

--- sketchybar/plugins/battery_status.sh
+++ sketchybar/plugins/battery_status.sh
@@ -2,17 +2,9 @@
 
 source "${CONFIG_DIR}/colors.sh"
 
-if [[ $(defaults read NSGlobalDomain AppleInterfaceStyle 2> /dev/null) == "Dark" ]]; then
-  icon_color="${WARNING_ICON_DARK}"
-  label_color="${WARNING_LABEL_DARK}"
-else
-  icon_color="${WARNING_ICON_LIGHT}"
-  label_color="${WARNING_LABEL_LIGHT}"
-fi
-
 case "${SENDER}" in
   "appearance_change")
-    sketchybar --set "${NAME}" icon.color="${icon_color}" label.color="${label_color}"
+    sketchybar --set "${NAME}" icon.color="${ICON_WARNING_COLOR}" label.color="${LABEL_WARNING_COLOR}"
     ;;
   *)
     battery_info="$(pmset -g batt)"
@@ -21,7 +13,7 @@ case "${SENDER}" in
     percentage="$(print "${battery_info}" | grep -Eo "\d+%" | cut -d% -f1)"
 
     if [[ -n "${is_discharging}" && "${percentage}" =~ ^[0-9]+$ && "${percentage}" -le 10 ]]; then
-      sketchybar --set "${NAME}" drawing=on label="${percentage}%" icon.color="${icon_color}" label.color="${label_color}"
+      sketchybar --set "${NAME}" drawing=on label="${percentage}%" icon.color="${ICON_WARNING_COLOR}" label.color="${LABEL_WARNING_COLOR}"
     else
       sketchybar --set "${NAME}" drawing=off
     fi
--- sketchybar/plugins/current_app.sh
+++ sketchybar/plugins/current_app.sh
@@ -2,6 +2,8 @@
 
 case "${NAME}" in
   "current_app_icon")
+    [[ ! "${BUNDLE_ID}" ]] && exit 0
+
     icon_path="${HOME}/.cache/sketchybar/app-icons/${BUNDLE_ID}.png"
     scale=0.5
 
@@ -18,23 +20,17 @@ case "${NAME}" in
     fi
 
     sketchybar --set "${NAME}" drawing=on background.image="${icon_path}" background.image.scale=${scale}
-  ;;
+    ;;
   "current_app_name")
     source "${CONFIG_DIR}/colors.sh"
 
-    if [[ $(defaults read NSGlobalDomain AppleInterfaceStyle 2> /dev/null) == "Dark" ]]; then
-      label_color="${LABEL_DARK}"
-    else
-      label_color="${LABEL_LIGHT}"
-    fi
-
     case "${SENDER}" in
       "appearance_change")
-        sketchybar --set "${NAME}" label.color="${label_color}"
-      ;;
+        sketchybar --set "${NAME}" label.color="${LABEL_COLOR}"
+        ;;
       "window_change")
-        sketchybar --set "${NAME}" label="${TITLE}" label.color="${label_color}"
-      ;;
+        sketchybar --set "${NAME}" label="${TITLE}" label.color="${LABEL_COLOR}"
+        ;;
     esac
-  ;;
+    ;;
 esac
