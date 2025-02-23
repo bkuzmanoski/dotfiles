e80f0315209bc69dcf2900af75782167ca4e533c
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2025-02-23 15:31:01 +1100

    Theme updates and optimizations

--- sketchybar/plugins/battery_status.sh
+++ sketchybar/plugins/battery_status.sh
@@ -2,20 +2,16 @@
 
 source "${CONFIG_DIR}/colors.sh"
 
-case "${SENDER}" in
-  "appearance_change")
-    sketchybar --set "${NAME}" icon.color="${ICON_WARNING_COLOR}" label.color="${LABEL_WARNING_COLOR}"
-    ;;
-  *)
-    battery_info="$(pmset -g batt)"
+if [[ "${SENDER}" == "appearance_change" ]]; then
+  sketchybar --set "${NAME}" icon.color="${FOREGROUND_WARNING_COLOR}" label.color="${FOREGROUND_WARNING_COLOR}"
+else
+  battery_info="$(pmset -g batt)"
+  is_discharging="$(print "${battery_info}" | grep "discharging")"
+  percentage="$(print "${battery_info}" | grep -Eo "\d+%" | cut -d% -f1)"
 
-    is_discharging="$(print "${battery_info}" | grep "discharging")"
-    percentage="$(print "${battery_info}" | grep -Eo "\d+%" | cut -d% -f1)"
-
-    if [[ -n "${is_discharging}" && "${percentage}" =~ ^[0-9]+$ && "${percentage}" -le 10 ]]; then
-      sketchybar --set "${NAME}" drawing=on label="${percentage}%" icon.color="${ICON_WARNING_COLOR}" label.color="${LABEL_WARNING_COLOR}"
-    else
-      sketchybar --set "${NAME}" drawing=off
-    fi
-    ;;
-esac
+  if [[ -n "${is_discharging}" && "${percentage}" =~ ^[0-9]+$ && "${percentage}" -le 10 ]]; then
+    sketchybar --set "${NAME}" drawing=on icon.color="${FOREGROUND_WARNING_COLOR}" label="${percentage}%" label.color="${FOREGROUND_WARNING_COLOR}"
+  else
+   sketchybar --set "${NAME}" drawing=off
+  fi
+fi
--- sketchybar/plugins/datetime.sh
+++ sketchybar/plugins/datetime.sh
@@ -2,5 +2,6 @@
 
 source "${CONFIG_DIR}/colors.sh"
 
-sketchybar --set date label="$(date '+%a %-d %b')" label.color="${LABEL_COLOR}"
-sketchybar --set time label="$(date '+%-I:%M %p' | tr '[:upper:]' '[:lower:]')" label.color="${LABEL_COLOR}"
+sketchybar \
+  --set date label="$(date '+%a %-d %b')" label.color="${FOREGROUND_COLOR}" \
+  --set time label="$(date '+%-I:%M %p' | tr '[:upper:]' '[:lower:]')" label.color="${FOREGROUND_COLOR}"
