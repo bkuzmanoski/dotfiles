8ff4a046a3d1359f29cc57ff153227e9956b27bb
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2025-02-25 15:48:14 +1100

    Improve handling of Finder when desktop is focused

--- hammerspoon/window_management/focus_after_close.lua
+++ hammerspoon/window_management/focus_after_close.lua
@@ -3,19 +3,17 @@ local module = {}
 local windowWatcher
 
 local function focusWindowOnScreen()
-  -- Brief delay to let focus update
   hs.timer.doAfter(0.1, function()
-    local focusedWindow = hs.window.focusedWindow()
     local targetScreen = hs.mouse.getCurrentScreen()
-
-    -- If the focus is already on the target screen (e.g. moved to another window of the same app), do nothing
-    if focusedWindow and focusedWindow:screen() == targetScreen then
+    local focusedWindow = hs.window.focusedWindow()
+    if focusedWindow and
+        focusedWindow:screen() == targetScreen and
+        not focusedWindow:title() == "" then
       return
     end
 
-    local windows = hs.window.orderedWindows()
     local candidate
-
+    local windows = hs.window.orderedWindows()
     for _, window in ipairs(windows) do
       if window:isVisible() and window:screen() == targetScreen then
         candidate = window
