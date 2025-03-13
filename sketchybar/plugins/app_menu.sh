bcd27ebd662990da0d3611e99825427bce81de50
bkuzmanoski <bkuzmanoski@icloud.com>
2025-03-14 10:37:04 +1100

    Syntax

--- sketchybar/plugins/app_menu.sh
+++ sketchybar/plugins/app_menu.sh
@@ -3,17 +3,17 @@
 source "${CONFIG_DIR}/variables.sh"
 
 case "${SENDER}" in
-  "mouse.entered")
+  mouse.entered)
     sketchybar \
       --animate "${ANIMATION_CURVE}" "${ANIMATION_DURATION}" \
       --set current_app_icon background.color="${BACKGROUND_HOVER_COLOR}"
     ;;
-  "mouse.exited")
+  mouse.exited)
     sketchybar \
       --animate "${ANIMATION_CURVE}" "${ANIMATION_DURATION}" \
       --set current_app_icon background.color=0x00ffffff
     ;;
-  "mouse.clicked")
+  mouse.clicked)
     sketchybar --set current_app_icon background.color="${BACKGROUND_ACTIVE_COLOR}"
     sleep $(print "${ANIMATION_DURATION} / 100" | bc -l)
     sketchybar --set current_app_icon background.color="${BACKGROUND_HOVER_COLOR}"
