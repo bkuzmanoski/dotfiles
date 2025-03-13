5b45b2d659b3bfef39900fec4a8ce93b4660c7ec
bkuzmanoski <bkuzmanoski@icloud.com>
2025-03-13 22:07:57 +1100

    Syntax

--- sketchybar/build_binaries.sh
+++ sketchybar/build_binaries.sh
@@ -5,7 +5,7 @@ BIN_DIR="${SOURCE_DIR}/bin"
 
 mkdir -p "${BIN_DIR}"
 
-local source_files=("${SOURCE_DIR}"/*.swift(N))
+source_files=("${SOURCE_DIR}"/*.swift(N))
 for source_file in "${source_files[@]}"; do
   target_path="${BIN_DIR}/$(basename "${source_file}" .swift)"
   if [ ! -x "${target_path}" ]; then
