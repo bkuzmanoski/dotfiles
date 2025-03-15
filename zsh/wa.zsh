17fb19ec9783ac50ca9c68b6696eab06b563cb8a
bkuzmanoski <bkuzmanoski@icloud.com>
2025-03-15 16:25:53 +1100

    Remove --silent handling

--- zsh/wa.zsh
+++ zsh/wa.zsh
@@ -5,28 +5,16 @@ wa() {
     return 1
   fi
 
-  local silent=0
-  if [[ "$1" == (-s|--silent) ]]; then
-    silent=1
-    shift
-  fi
-
   local wa_response wa_status
   wa_response=$(curl --silent "https://api.wolframalpha.com/v1/result?appid=${WA_APPID}&units=metric&" --data-urlencode "i=$*")
   wa_status=$?
   if [[ ${wa_status} -ne 0 ]]; then
-    [[ ${silent} -eq 0 ]] && print "Error: Failed to connect to Wolfram|Alpha (curl status: ${wa_status})."
+    print "Error: Failed to connect to Wolfram|Alpha (curl status: ${wa_status})."
     return 1
   fi
 
-  case "${wa_response}" in
-    "No short answer available"|"Wolfram|Alpha did not understand your input")
-      [[ ${silent} -eq 0 ]] && print "${wa_response}"
-      return 1
-      ;;
-    *)
-      print "${wa_response}"
-      return 0
-      ;;
-  esac
+  print "${wa_response}"
+
+  [[ "${wa_response}" == "No short answer available" || "${wa_response}" == "Wolfram|Alpha did not understand your input" ]] && return 1
+  return 0
 }
