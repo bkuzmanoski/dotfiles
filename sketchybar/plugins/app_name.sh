6d5b1dddb128b99525df2c400c33f78bf17d1b9b
bkuzmanoski <bkuzmanoski@icloud.com>
2025-03-24 16:05:36 +1100

    Add window title

--- sketchybar/plugins/app_name.sh
+++ sketchybar/plugins/app_name.sh
@@ -3,13 +3,14 @@
 source "${CONFIG_DIR}/variables.sh"
 
 case "${SENDER}" in
-  appearance_change)
+  appearance_changed)
     sketchybar \
       --animate "${ANIMATION_CURVE}" "${ANIMATION_DURATION}" \
-      --set current_app_name label.color="${TEXT_DEFAULT_COLOR}"
+      --set app_name label.color="${TEXT_DEFAULT_COLOR}"
     ;;
-  app_change)
-    arguments=(--set current_app_name label="${APP_NAME}" label.color="${TEXT_DEFAULT_COLOR}")
+  app_activated)
+    arguments=(--set app_name label="${APP_NAME}" label.color="${TEXT_DEFAULT_COLOR}")
+    arguments+=(--set window_title label="${WINDOW_TITLE}" label.color="${TEXT_DEFAULT_COLOR}")
 
     icon_path="${HOME}/.cache/sketchybar/icons/${BUNDLE_ID}.png"
     return_value=0
@@ -19,9 +20,9 @@ case "${SENDER}" in
     fi
 
     if [[ ${return_value} -eq 0 ]]; then
-      arguments+=(--set current_app_icon drawing=on background.image="${icon_path}")
+      arguments+=(--set app_icon drawing=on background.image="${icon_path}")
     else
-      arguments+=(--set current_app_icon drawing=off)
+      arguments+=(--set app_icon drawing=off)
     fi
 
     sketchybar "${arguments[@]}"
--- sketchybar/plugins/battery_status.sh
+++ sketchybar/plugins/battery_status.sh
@@ -2,18 +2,21 @@
 
 source "${CONFIG_DIR}/variables.sh"
 
-if [[ "${SENDER}" == "appearance_change" ]]; then
-  sketchybar \
-    --animate "${ANIMATION_CURVE}" "${ANIMATION_DURATION}" \
-    --set battery_status icon.color="${TEXT_WARNING_COLOR}" label.color="${TEXT_WARNING_COLOR}"
-else
-  battery_info="$(pmset -g batt)"
-  is_discharging="$(print "${battery_info}" | grep "discharging")"
-  percentage="$(print "${battery_info}" | grep -Eo "\d+%" | cut -d% -f1)"
+case "${SENDER}" in
+  appearance_changed)
+    sketchybar \
+      --animate "${ANIMATION_CURVE}" "${ANIMATION_DURATION}" \
+      --set battery_status icon.color="${TEXT_WARNING_COLOR}" label.color="${TEXT_WARNING_COLOR}"
+      ;;
+  *)
+    battery_info="$(pmset -g batt)"
+    is_discharging="$(print "${battery_info}" | grep "discharging")"
+    percentage="$(print "${battery_info}" | grep -Eo "\d+%" | cut -d% -f1)"
 
-  if [[ -n "${is_discharging}" && "${percentage}" =~ ^[0-9]+$ && "${percentage}" -le 20 ]]; then
-    sketchybar --set battery_status drawing=on icon.color="${TEXT_WARNING_COLOR}" label="${percentage}%" label.color="${TEXT_WARNING_COLOR}" update_freq="${UPDATE_FREQUENCY}"
-  else
-    sketchybar --set battery_status drawing=off update_freq="${UPDATE_FREQUENCY}"
-  fi
-fi
+    if [[ -n "${is_discharging}" && "${percentage}" =~ ^[0-9]+$ && "${percentage}" -le 20 ]]; then
+      sketchybar --set battery_status drawing=on icon.color="${TEXT_WARNING_COLOR}" label="${percentage}%" label.color="${TEXT_WARNING_COLOR}" update_freq="${UPDATE_FREQUENCY}"
+    else
+      sketchybar --set battery_status drawing=off update_freq="${UPDATE_FREQUENCY}"
+    fi
+    ;;
+esac
