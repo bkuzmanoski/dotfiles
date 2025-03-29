f3292c6c242cd7da4185d2de50a2be5d84dba01d
bkuzmanoski <bkuzmanoski@icloud.com>
2025-03-29 16:05:48 +1100

    Ignore right-clicks on interactive Sketchybar items

--- sketchybar/plugins/app_icon.sh
+++ sketchybar/plugins/app_icon.sh
@@ -14,7 +14,10 @@ case "${SENDER}" in
       --set app_icon background.color=0x00ffffff
     ;;
   mouse.clicked)
+    if [[ "${BUTTON}" != "left" ]]; then return; fi
+
     sketchybar --set app_icon background.color="${BACKGROUND_ACTIVE_COLOR}"
+    open -g "hammerspoon://showAppMenu?x=8&y=45"
     sleep $(print "scale=2; ${ANIMATION_DURATION} / 100" | bc)
     sketchybar --set app_icon background.color="${BACKGROUND_HOVER_COLOR}"
     ;;
