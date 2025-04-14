af4e87cca705b7ee412f9aa075468ead557239e5
bkuzmanoski <bkuzmanoski@icloud.com>
2025-04-14 14:16:04 +1000

    Add Swift format/lint config

--- hammerspoon/helpers/ExtractURLs.swift
+++ hammerspoon/helpers/ExtractURLs.swift
@@ -1,19 +1,15 @@
 import Foundation
 
 let inputData = FileHandle.standardInput.availableData
-guard
-  let inputText = String(data: inputData, encoding: .utf8),
-  !inputText.isEmpty
-else {
+guard let inputText = String(data: inputData, encoding: .utf8), !inputText.isEmpty else {
   print("No input provided. Pipe some text into this command.")
   exit(1)
 }
 
-let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
-let matches = detector.matches(
-  in: inputText, options: [], range: NSRange(location: 0, length: inputText.utf16.count))
-for match in matches {
-  if let url = match.url?.absoluteString {
-    print(url)
-  }
+guard let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue) else {
+  print("Could not create detector.")
+  exit(1)
 }
+
+let matches = detector.matches(in: inputText, options: [], range: NSRange(location: 0, length: inputText.utf16.count))
+for match in matches { if let url = match.url?.absoluteString { print(url) } }
