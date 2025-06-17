d62b0a89dcae902ab31bfc8a06369bfa1306146c
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2025-06-18 09:58:27 +1000

    Update Chrome NSUserKeyEquivalents

--- setup.sh
+++ setup.sh
@@ -195,8 +195,6 @@ _defaults_write com.google.Chrome NSUserKeyEquivalents -dict-add "Developer Tool
 _defaults_write com.google.Chrome NSUserKeyEquivalents -dict-add "Email Link" "\U0000" # Remove keyboard shortcut for Email Link (conflicts with ⇧⌘I)
 _defaults_write com.google.Chrome NSUserKeyEquivalents -dict-add "New Tab to the Right" "@t" # Map New Tab to the Right keyboard shortcut to ⌘T
 _defaults_write com.google.Chrome NSUserKeyEquivalents -dict-add "New Tab" "\U0000" # Remove keyboard shortcut for New Tab (conflicts with ⌘T)
-_defaults_write com.google.Chrome NSUserKeyEquivalents -dict-add "Select Next Tab" "\$@d" # Map Select Next Tab keyboard shortcut to ⇧⌘D
-_defaults_write com.google.Chrome NSUserKeyEquivalents -dict-add "Select Previous Tab" "\$@a" # Map Select Previous Tab keyboard shortcut to ⇧⌘A
 _defaults_write com.lwouis.alt-tab-macos "NSStatusItem Visible Item-0" -int 0 # Hide menu bar icon
 _defaults_write com.lwouis.alt-tab-macos appearanceStyle -int 2 # Set appearance to "Titles"
 _defaults_write com.lwouis.alt-tab-macos appearanceVisibility -int 1 # Set appearance visibility to "High"
