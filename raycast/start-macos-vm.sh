546ec82f10b6360959afadc3e521406a31636a84
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2026-01-30 11:00:48 +1100

    UTM Raycast shortcut

---
+++ raycast/start-macos-vm.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# @raycast.title Start macOS Sequoia VM
+# @raycast.packageName UTM
+# @raycast.icon icons/utm.png
+# @raycast.mode silent
+# @raycast.schemaVersion 1
+
+/Applications/UTM.app/Contents/MacOS/utmctl start "macOS Sequoia"
