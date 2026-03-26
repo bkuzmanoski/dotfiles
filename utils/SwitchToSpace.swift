27b4a7ba2a591e5370ca35c1fd61751c8592fb30
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2026-03-27 09:42:37 +1100

    Tweak

--- utils/SwitchToSpace.swift
+++ utils/SwitchToSpace.swift
@@ -78,6 +78,7 @@ final class SingletonLock {
   deinit {
     flock(lockFileDescriptor, LOCK_UN)
     close(lockFileDescriptor)
+
     try? FileManager.default.removeItem(atPath: lockFilePath)
   }
 }
