5c49b79921a92194408864ee45e7f95b525a36ef
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2026-03-30 11:33:55 +1100

    Tweak keybinds

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -42,8 +42,8 @@ modules.systemHotkeys = require("modules/system_hotkeys").init({
   toggleControlCenter = { modifiers = settings.hyperKey, key = "i" }
 })
 modules.switchToSpace = require("modules/switch_to_space").init({
-  left = { modifiers = settings.hyperKey, key = "[" },
-  right = { modifiers = settings.hyperKey, key = "]" },
+  left = { modifiers = settings.hyperKey, key = "o" },
+  right = { modifiers = settings.hyperKey, key = "p" },
   index = { modifiers = settings.hyperKey, maximumSpaces = settings.numberOfSpaces }
 })
 modules.adjustNewWindowPosition = require("modules/adjust_new_window_position").init({
@@ -105,8 +105,8 @@ modules.moveWindowToScreen = require("modules/move_window_to_screen").init({
 modules.focusWindow = require("modules/focus_window").init({
   hotkeys = {
     frontmost = { modifiers = { "shift", "command" }, key = "return" },
-    left = { modifiers = settings.hyperKey, key = "o" },
-    right = { modifiers = settings.hyperKey, key = "p" }
+    left = { modifiers = settings.hyperKey, key = "[" },
+    right = { modifiers = settings.hyperKey, key = "]" }
   }
 })
 modules.focusWindowOnScreen = require("modules/focus_window_on_screen").init()
