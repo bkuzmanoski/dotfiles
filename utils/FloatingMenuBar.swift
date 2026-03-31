4c91b28dc64f2a3f2967716d9dacd4f213b4c39b
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2026-03-31 17:05:52 +1100

    Tweak

--- utils/FloatingMenuBar.swift
+++ utils/FloatingMenuBar.swift
@@ -41,7 +41,7 @@ struct Command {
   }
 }
 
-final class SingletonLock {
+final class SingleInstanceLock {
   enum Error: Swift.Error, LocalizedError {
     case instanceAlreadyRunning
     case failedToAcquireLock(errno: Int32)
@@ -376,11 +376,11 @@ final class AppMenu {
 final class AppDelegate: NSObject, NSApplicationDelegate {
   private(set) var eventTap: CFMachPort?
 
-  private let singletonLock: SingletonLock
+  private let singleInstanceLock: SingleInstanceLock
   private var runLoopSource: CFRunLoopSource?
 
-  init(singletonLock: SingletonLock) {
-    self.singletonLock = singletonLock
+  init(singleInstanceLock: SingleInstanceLock) {
+    self.singleInstanceLock = singleInstanceLock
     super.init()
   }
 
@@ -490,14 +490,14 @@ func eventTapCallback(
 }
 
 do {
-  let singletonLock = try SingletonLock()
-  let delegate = AppDelegate(singletonLock: singletonLock)
+  let singleInstanceLock = try SingleInstanceLock()
+  let delegate = AppDelegate(singleInstanceLock: singleInstanceLock)
   let application = NSApplication.shared
   application.delegate = delegate
   application.setActivationPolicy(.prohibited)
   application.run()
 
-} catch SingletonLock.Error.instanceAlreadyRunning {
+} catch SingleInstanceLock.Error.instanceAlreadyRunning {
   let arguments = Array(CommandLine.arguments.dropFirst())
 
   guard !arguments.isEmpty else {
