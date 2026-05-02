e5eb295e36a7c34f4475b7532eccdc1214e9c0c2
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2026-05-02 18:40:37 +1000

    Add script to toggle Focus Follows Mouse

---
+++ raycast/toggle-focus-follows-mouse.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# @raycast.title Toggle Focus Follows Mouse
+# @raycast.packageName Utilities
+# @raycast.icon icons/toggle-focus-follows-mouse.png
+# @raycast.mode silent
+# @raycast.schemaVersion 1
+
+${HOME}/.dotfiles/utils/run_util.sh --background FocusFollowsMouse toggle
