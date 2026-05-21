e8b00daac446a0c3706e58f65abbb5637954abb4
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2026-05-21 15:54:48 +1000

    SampleColor: Revert to NSColorSampler

--- raycast/sample-color.sh
+++ raycast/sample-color.sh
@@ -4,8 +4,7 @@
 # @raycast.packageName Utilities
 # @raycast.icon icons/sample-color.png
 # @raycast.argument1 { "type": "dropdown", "placeholder": "Output format", "data": [{"title": "Hexadecimal", "value": "--hex"}, {"title": "RGB", "value": "--rgb"}] }
-# @raycast.argument2 { "type": "dropdown", "placeholder": "Color space", "data": [{"title": "sRGB", "value": "--srgb"}, {"title": "Native", "value": "--native"}] }
 # @raycast.mode silent
 # @raycast.schemaVersion 1
 
-${HOME}/.dotfiles/utils/run_util.sh SampleColor "$@"
+${HOME}/.dotfiles/utils/run_util.sh SampleColor "$1"
