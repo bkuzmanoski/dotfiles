85c643af02d4dc079f834be8e7596201a0fde7f7
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2026-08-20 18:11:44 +1000

    Fix missing whitespace in dropdown options for Send Chrome Tabs to Feedbin script

--- raycast/send-chrome-tabs-to-feedbin.sh
+++ raycast/send-chrome-tabs-to-feedbin.sh
@@ -3,8 +3,8 @@
 # @raycast.title Send Chrome Tabs to Feedbin
 # @raycast.packageName Utilities
 # @raycast.icon icons/send-chrome-tabs-to-feedbin.png
-# @raycast.argument1 { "type": "dropdown", "placeholder": "Tabs", "data": [{"title": "Current Tab", "value": "--active-only"}, {"title": "All Tabs", "value": ""}] }
-# @raycast.argument2 { "type": "dropdown", "placeholder": "After Sending", "data": [{"title": "Close Tab(s)", "value": "--close-sent"}, {"title": "Keep Tab(s) Open", "value": ""}] }
+# @raycast.argument1 { "type": "dropdown", "placeholder": "Tabs", "data": [{"title": "Current Tab", "value": "--active-only"}, {"title": "All Tabs", "value": " "}] }
+# @raycast.argument2 { "type": "dropdown", "placeholder": "After Sending", "data": [{"title": "Close Tab(s)", "value": "--close-sent"}, {"title": "Keep Tab(s) Open", "value": " "}] }
 # @raycast.mode silent
 # @raycast.schemaVersion 1
 
