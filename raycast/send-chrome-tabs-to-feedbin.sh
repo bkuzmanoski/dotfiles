838d6fc3398030a00b2aa4b4cdb99085e75292da
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2026-09-15 13:28:08 +1000

    Update Send Chrome Tabs to Feedbin script for Raycast V2

--- raycast/send-chrome-tabs-to-feedbin.sh
+++ raycast/send-chrome-tabs-to-feedbin.sh
@@ -3,9 +3,9 @@
 # @raycast.title Send Chrome Tabs to Feedbin
 # @raycast.packageName Utilities
 # @raycast.icon icons/send-chrome-tabs-to-feedbin.png
-# @raycast.argument1 { "type": "dropdown", "placeholder": "Tabs", "data": [{"title": "Current Tab", "value": "--active-only"}, {"title": "All Tabs", "value": " "}] }
-# @raycast.argument2 { "type": "dropdown", "placeholder": "After Sending", "data": [{"title": "Close Tab(s)", "value": "--close-sent"}, {"title": "Keep Tab(s) Open", "value": " "}] }
+# @raycast.argument1 { "type": "dropdown", "placeholder": "Tabs", "data": [{"title": "Current Tab", "value": "--active-only"}, {"title": "All Tabs", "value": "none"}] }
+# @raycast.argument2 { "type": "dropdown", "placeholder": "After Sending", "data": [{"title": "Close Tab(s)", "value": "--close-sent"}, {"title": "Keep Tab(s) Open", "value": "none"}] }
 # @raycast.mode silent
 # @raycast.schemaVersion 1
 
-${HOME}/.dotfiles/utils/run_util.sh SendChromeTabs "bwilw@feedb.in" "$1" "$2"
+${HOME}/.dotfiles/utils/run_util.sh SendChromeTabs "bwilw@feedb.in" "${@:#none}"
